3629732fb44a5b13128b748789b4a16ccfe10c33
Kelvin Chua <chuakid@gmail.com>
2021-12-13 21:37:24 +0800

    Highlighting selected project

--- src/components/LoggedIn/Sidebar/ProjectLink.tsx
+++ src/components/LoggedIn/Sidebar/ProjectLink.tsx
@@ -8,7 +8,7 @@ const ProjectLink = ({ project }: { project: Project }) => {
     let selectedProjectId: number = params.projectId ? parseInt(params.projectId) : -1
 
     return (
-        <Link fontSize="xl" color="white" fontWeight={selectedProjectId === project.id ? "bold" : "thin"} w="100%" as={RouterLink} to={"/project/" + project.id}>{project.name}</Link>
+        <Link fontSize="xl" bgColor={selectedProjectId === project.id ? "blue.700" : "transparent"} px="2" rounded="md" color="white" fontWeight={selectedProjectId === project.id ? "bold" : "thin"} w="100%" as={RouterLink} to={"/project/" + project.id}>{project.name}</Link>
     )
 }
 

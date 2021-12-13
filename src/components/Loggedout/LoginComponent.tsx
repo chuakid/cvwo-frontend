e4537c91c8b8dbcc9b7ce89f6303ef0865cd5b4a
Kelvin Chua <chuakid@gmail.com>
2021-12-13 13:07:40 +0800

    Registration

--- src/components/Loggedout/LoginComponent.tsx
+++ src/components/Loggedout/LoginComponent.tsx
@@ -1,24 +1,50 @@
 import React, { useState } from 'react'
 import { Button, Center, Flex, FormControl, FormLabel, Stack, useToast } from "@chakra-ui/react"
 import FormInput from '../FormInput';
-import { login } from '../../services/userservices';
+import { login, register } from '../../services/userservices';
 
 const LoginComponent = () => {
     const formBg = "white"
     const toast = useToast()
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
+    const [loading, setLoading] = useState(false)
+
     function handleLogin() {
+        setLoading(true)
         login(username, password)
-            .then((result) => { console.log(result) })
+            .then((result) => {
+                setLoading(false)
+                console.log(result)
+            })
             .catch(error => {
                 toast({
                     "title": "Invalid account",
                     "duration": 3000,
                     "status": 'error'
                 })
+            }).finally(() => {
+                setLoading(false)
             })
     }
+    function handleRegister() {
+        setLoading(true)
+        register(username, password)
+            .then((result) => {
+                setLoading(false)
+                console.log(result)
+            })
+            .catch(error => {
+                toast({
+                    "title": "Username taken",
+                    "duration": 3000,
+                    "status": 'error'
+                })
+            }).finally(() => {
+                setLoading(false)
+            })
+    }
+
     return (
         <Center h="100vh">
             <Flex as="form" rounded="md" padding="5" backgroundColor={formBg} boxShadow="lg" border="1px" borderColor="gray.200">
@@ -26,13 +52,16 @@ const LoginComponent = () => {
                     <FormControl>
                         <FormLabel>Username</FormLabel>
                         <FormInput fn={(e: React.ChangeEvent<HTMLInputElement>) => setUsername(e.target.value)} />
-
                     </FormControl>
                     <FormControl>
                         <FormLabel>Password</FormLabel>
                         <FormInput fn={(e: React.ChangeEvent<HTMLInputElement>) => setPassword(e.target.value)} type="password" />
                     </FormControl>
-                    <Button bgColor="blue.200" _hover={{ bgColor: "blue.500" }} onClick={handleLogin} width="100%" marginTop="2">Login</Button>
+                    <FormControl>
+                        <Button disabled={loading} bgColor="blue.200" _hover={{ bgColor: "blue.500" }} onClick={handleLogin} width="100%" marginTop="2">
+                            Login</Button>
+                        <Button disabled={loading} bgColor="red.200" _hover={{ bgColor: "red.500" }} onClick={handleRegister} width="100%" marginTop="2">Register</Button>
+                    </FormControl>
 
                 </Stack>
 
--- src/services/userservices.tsx
+++ src/services/userservices.tsx
@@ -6,3 +6,10 @@ export function login(username: string, password: string) {
         "password": password
     })
 }
+
+export function register(username: string, password: string) {
+    return API.post("/register", {
+        "username": username,
+        "password": password
+    })
+}

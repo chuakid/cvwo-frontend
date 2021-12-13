7311c2942aaefbc66091f542babecd22f502b3f2
Kelvin Chua <chuakid@gmail.com>
2021-12-13 20:49:45 +0800

    Sidebar sizing

--- src/components/LoggedIn/Sidebar/Sidebar.tsx
+++ src/components/LoggedIn/Sidebar/Sidebar.tsx
@@ -11,7 +11,7 @@ const Sidebar = () => {
         dispatch(setJwt(""))
     }
     return (
-        <Flex p="3" height="100vh" w="10%" bgColor="blue.800" flexDirection="column" justifyContent="space-between">
+        <Flex p="3" w={{ base: "30%", md: "25%", lg: "15%" }} height="100vh" bgColor="blue.800" flexDirection="column" justifyContent="space-between">
             <ProjectsComponent />
             <Button onClick={handleLogout} mb="2" mx="2">Logout</Button>
         </Flex>

36049bdc99b5f09ca367d9338ce31c88d7af9657
Kelvin Chua <chuakid@gmail.com>
2021-12-13 18:59:25 +0800

    Check if project exists

--- src/components/LoggedIn/ProjectComponent.tsx
+++ src/components/LoggedIn/ProjectComponent.tsx
@@ -1,15 +1,21 @@
-import React from 'react'
-import { useParams } from "react-router-dom";
+import React, { useEffect } from 'react'
+import { useNavigate, useParams } from "react-router-dom";
 import { useAppSelector } from '../../store/typedHooks';
 
 const ProjectComponent = () => {
     let params = useParams()
-    let id: number;
-    if (params.projectId !== undefined) {
-        id = parseInt(params.projectId)
-    }
+    const navigate = useNavigate();
+    let id: number = params.projectId ? parseInt(params.projectId) : -1
+
     //Get project
     const project = useAppSelector(state => state.projects.find((x) => x.id === id))
+    useEffect(() => {
+        if (!project) {
+            navigate("/");
+        }
+    }, [project, navigate])
+
+
     return (
         <div>
             {project?.name}

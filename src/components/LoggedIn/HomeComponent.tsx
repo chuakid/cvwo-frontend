cfddb52ccaf9dc6307dac4b9a11d488855386107
Kelvin Chua <chuakid@gmail.com>
2021-12-14 18:11:15 +0800

    removed unused import

--- src/components/LoggedIn/HomeComponent.tsx
+++ src/components/LoggedIn/HomeComponent.tsx
@@ -1,4 +1,4 @@
-import { Box, Flex } from '@chakra-ui/react'
+import { Flex } from '@chakra-ui/react'
 import { Outlet } from 'react-router-dom'
 import Navbar from './Navbar/Navbar'
 import Sidebar from './Sidebar/Sidebar'

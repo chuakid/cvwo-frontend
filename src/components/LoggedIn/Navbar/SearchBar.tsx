eb9da7b9e53c97decf2751060bb2d8414553819c
Kelvin Chua <chuakid@gmail.com>
2021-12-15 15:21:49 +0800

    search bar upgrade

--- src/components/LoggedIn/Navbar/SearchBar.tsx
+++ src/components/LoggedIn/Navbar/SearchBar.tsx
@@ -3,11 +3,33 @@ import { SearchIcon } from '@chakra-ui/icons'
 import { Box, Divider, Flex, Input, Modal, ModalBody, ModalContent, ModalOverlay, useDisclosure } from '@chakra-ui/react'
 import { useAppSelector } from '../../../store/typedHooks'
 import SearchItem from './SearchItem'
+import { useNavigate } from 'react-router-dom'
 
 const SearchBar = () => {
     const { isOpen, onOpen, onClose } = useDisclosure()
     const [search, setSearch] = useState("")
+    const [selected, setSelected] = useState(0)
+    const navigate = useNavigate()
     let searchedProjects = useAppSelector(state => Object.values(state.projects).filter((project) => project.name.includes(search)))
+
+    function handleKeyUp(e: React.KeyboardEvent<HTMLInputElement>) {
+        switch (e.key) {
+            case "ArrowUp": {
+                setSelected(selected === 0 ? selected : selected - 1)
+                break
+            }
+            case "ArrowDown": {
+                setSelected(selected === searchedProjects.length - 1 ? selected : selected + 1)
+                break
+            }
+            case "Enter": {
+                navigate("/project/" + searchedProjects[selected].id)
+                onClose()
+                break
+            }
+        }
+    }
+
     return (
         <>
             <Flex onClick={onOpen} alignItems="center" bgColor="blue.600" rounded="lg" px="3" w="50%">
@@ -18,10 +40,13 @@ const SearchBar = () => {
             <Modal isOpen={isOpen} onClose={onClose}>
                 <ModalOverlay />
                 <ModalContent>
-                    <Input fontSize="2xl" border="none" placeholder="Search" h="20" value={search} onInput={(e: React.ChangeEvent<HTMLInputElement>) => setSearch(e.target.value)} />
+                    <Flex alignItems="center">
+                        <SearchIcon ml="2" h="6" w="6" />
+                        <Input _focus={{ outline: "none" }} flex="1" fontSize="2xl" border="none" placeholder="Search" rounded="none" py="7" value={search} onInput={(e: React.ChangeEvent<HTMLInputElement>) => setSearch(e.target.value)} onKeyUp={handleKeyUp} />
+                    </Flex>
                     <Divider mb="3"></Divider>
-                    <ModalBody>
-                        {searchedProjects.map((project) => <SearchItem key={project.id} closeModal={onClose} project={project} />)}
+                    <ModalBody px="3">
+                        {searchedProjects.map((project, index) => <SearchItem key={project.id} selected={index === selected} closeModal={onClose} project={project} />)}
                     </ModalBody>
                 </ModalContent>
             </Modal>
--- src/components/LoggedIn/Navbar/SearchItem.tsx
+++ src/components/LoggedIn/Navbar/SearchItem.tsx
@@ -2,14 +2,16 @@ import { LinkBox, LinkOverlay } from '@chakra-ui/react'
 import { Link as RouterLink } from 'react-router-dom'
 import React from 'react'
 import Project from '../../../types/Project'
-import Task from '../../../types/Task'
 
-const SearchItem = ({ closeModal, project, task }: { closeModal: React.MouseEventHandler<HTMLAnchorElement>, project: Project, task?: Task }) => {
+const SearchItem = ({ closeModal, project, selected }: { closeModal: React.MouseEventHandler<HTMLAnchorElement>, project: Project, selected: boolean }) => {
+    const styles = {
+        bgColor: selected ? "blue.600" : "gray.200",
+        color: selected ? "white" : "black"
+    }
     return (
         <LinkBox>
-            <LinkOverlay key={project.id} onClick={closeModal} rounded="lg" _hover={{ bgColor: "blue.600", color: "white" }} as={RouterLink} display="block" fontSize="2xl" bgColor="gray.200" mb="2" p="2" width="100%"
+            <LinkOverlay key={project.id} onClick={closeModal} rounded="lg" as={RouterLink} display="block" fontSize="2xl" color={styles.color} bgColor={styles.bgColor} mb="2" p="2" width="100%"
                 to={"/project/" + project.id}>{project.name}
-                {task && <>{task.description}</>}
             </LinkOverlay></LinkBox>
     )
 }

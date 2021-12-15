f1536882c7cd5acefdefd070bd2fd53494f765b6
Kelvin Chua <chuakid@gmail.com>
2021-12-15 16:57:18 +0800

    add task button

--- src/components/LoggedIn/Project/CreateTaskInput.tsx
+++ src/components/LoggedIn/Project/CreateTaskInput.tsx
@@ -1,18 +1,19 @@
-import { Input } from '@chakra-ui/react'
+import { Button, Input, InputGroup, InputRightAddon } from '@chakra-ui/react'
 import { createTask } from '../../../services/taskservices'
-import React from 'react'
+import React, { useState } from 'react'
 import { useAppDispatch } from '../../../store/typedHooks'
 import { addTaskToProject } from '../../../store/projectSlice'
 import Task from '../../../types/Task'
 
 const CreateTaskInput = ({ projectid }: { projectid: number }) => {
     const dispatch = useAppDispatch()
-    function handleCreateTask(taskDesc: string) {
-        createTask(projectid, taskDesc).then(
+    const [task, setTask] = useState("")
+    function handleCreateTask() {
+        createTask(projectid, task).then(
             (result) => {
                 dispatch(addTaskToProject({
                     id: result.data.id,
-                    description: taskDesc,
+                    description: task,
                     projectid: projectid
                 } as Task))
             }
@@ -20,11 +21,18 @@ const CreateTaskInput = ({ projectid }: { projectid: number }) => {
 
     }
     return (
-        <Input w="64" boxShadow="md" borderColor="gray.400" border="1px" _placeholder={{color: "gray.400"}} onKeyUp={(e) => {
-            if (e.key === "Enter") {
-                handleCreateTask((e.target as HTMLInputElement).value)
-            }
-        }} placeholder="Create a task"></Input>
+        <InputGroup width="auto" boxShadow="lg">
+            <Input w="64"  _placeholder={{ color: "gray.400" }} onInput={(e) => {
+                setTask(e.currentTarget.value)
+            }} onKeyUp={(e) => {
+                if (e.key === "Enter") {
+                    handleCreateTask()
+                }
+            }} placeholder="Create a task"></Input>
+            <InputRightAddon colorScheme="gray" as={Button} onClick={handleCreateTask}>
+                Add Task
+            </InputRightAddon>
+        </InputGroup>
     )
 }
 

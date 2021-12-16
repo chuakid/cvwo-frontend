63fa2ead421ec6f29d53a5e8ba791bf0c1272056
Kelvin Chua <chuakid@gmail.com>
2021-12-16 17:00:53 +0800

    Loading spinner

--- src/components/LoggedIn/Project/ProjectComponent.tsx
+++ src/components/LoggedIn/Project/ProjectComponent.tsx
@@ -1,5 +1,5 @@
-import { Box, Flex, Wrap, WrapItem } from '@chakra-ui/react';
-import React, { useEffect } from 'react'
+import { Box, Center, Flex, Spinner, Wrap, WrapItem } from '@chakra-ui/react';
+import React, { useEffect, useState } from 'react'
 import { useNavigate, useParams } from "react-router-dom";
 import { getProject } from '../../../services/projectservices';
 import { editProject } from '../../../store/projectSlice';
@@ -13,11 +13,14 @@ const ProjectComponent = () => {
     let params = useParams()
     const navigate = useNavigate();
     let id: number = params.projectId ? parseInt(params.projectId) : -1
+    const [loading, setLoading] = useState(true)
     const dispatch = useAppDispatch()
     //Get updated project
     useEffect(() => {
+        setLoading(true)
         getProject(id).then(result => {
             dispatch(editProject(result.data))
+            setLoading(false)
         }).catch(e => navigate("/"))
     }, [navigate, dispatch, id])
     const project = useAppSelector(state => state.projects[id])
@@ -25,15 +28,17 @@ const ProjectComponent = () => {
     const sortedTasks = project?.tasks?.map(x => x)
         .sort((a, b) => { return a.completed && b.completed ? 0 : a.completed ? 1 : -1 })
 
-    return ( 
+    return (
         <Box flex="1" p="3">
             <Flex gap="2">
                 <CreateTaskInput projectid={project ? project.id : -1} />
                 <DeleteProjectButton id={project?.id} />
             </Flex>
-            <Wrap mt="2">
-                {sortedTasks?.map((task) => <WrapItem key={task.id}><TaskComponent task={task}></TaskComponent></WrapItem>)}
-            </Wrap>
+            {loading ? <Center fontSize="4xl" h="100vh"><Spinner mr="2" size="xl" />Loading</Center> :
+                <Wrap mt="2">
+                    {sortedTasks?.map((task) => <WrapItem key={task.id}><TaskComponent task={task}></TaskComponent></WrapItem>)}
+                </Wrap>
+            }
         </Box>
     )
 }

e296edab824bd1e3a2b4b995b2e52e4541bd8095
Kelvin Chua <chuakid@gmail.com>
2021-12-16 21:57:14 +0800

    log out on token expiry

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -1,10 +1,22 @@
-import axios from "axios"; 
+import axios, { AxiosError } from "axios";
 import { API_URL } from "../globals"
+import { logout } from "../helpers/userhelpers";
 
 export let API = axios.create({
     baseURL: API_URL
 });
 
-export function setAPIToken(token: string) {    
+export function setAPIToken(token: string) {
     API.defaults.headers.common['Authorization'] = "Bearer " + token;
 }
+
+API.interceptors.response.use(function (config) {
+    return config;
+}, function (error: AxiosError) {
+        if(error.response?.data === "Invalid token") {
+            logout()
+            return
+        }
+        return Promise.reject(error);
+    
+});
